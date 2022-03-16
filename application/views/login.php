d95b0474cb5727b7a64a21c858811429b3aa78d8
Carlos Alejandro <emiramirez991@gmail.com>
2022-03-16 12:18:50 -0600

    Configuraciones iniciales del codeigniter y se agrege bootstrap y jquery

--- application/views/login.php
+++ application/views/login.php
@@ -5,8 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap/bootstrap.min.css');?>">
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-7">
+                <h1>Hola</h1>
+            </div>
+        </div>
+    </div>
+<script src="<?= base_url('assets/css/bootstrap/bootstrap.min.js');?>"></script>
+<script src="<?= base_url('assets/js/jquery/jquery-3.6.0.min.js');?>"></script>
 </body>
 </html>
