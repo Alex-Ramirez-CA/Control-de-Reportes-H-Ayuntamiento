1091a9fa6224050ab1e5f48667b569c80ff3e077
Carlos Alejandro <emiramirez991@gmail.com>
2022-03-18 16:16:00 -0600

    manejo de sesiones de usuario

---
+++ application/controllers/Cliente.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Cliente extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+		$this->load->library(array('session'));
+	}
+
+	public function index()
+	{
+		$data = array(
+			'head' => $this->load->view('layout/head', '', TRUE),
+			'footer' => $this->load->view('layout/footer', '', TRUE),
+		);
+
+        if($this->session->userdata('rol') == 0) {
+        	$this->load->view('v_cliente', $data);
+        } else {
+            // Si no hay datos de sesion redireccionar a login
+            redirect('login');
+        }
+		
+	}
+
+}
