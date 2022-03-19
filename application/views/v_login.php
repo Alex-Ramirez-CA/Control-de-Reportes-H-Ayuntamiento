b06f0ca836b8cb5407d62094b22d2476d5ccae47
Luis Gerardo Sanchez Diaz <liger3799@gmail.com>
2022-03-18 18:19:57 -0600

    Maquetación de la pantalla de logueo

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -1,19 +1,19 @@
 <?= $head ?>
     <div class="container">
-        <div class="row justify-content-lg-center align-items-lg-center">
-            <div class="col-lg-6 align-self-center">
+        <div class="row justify-content-center align-items-center" style="height: 100vh">
+            <div class="col-lg-4 col-md-6 col-sm-7 text-center">
                  <form action="<?= base_url('login/validar') ?>" method="POST" id="frm_login">
-                    <div class="form-group">
-                        <h1>Login</h1>
+                    <div class="form-group pb-2">
+                        <h1>Inicio de sesión</h1>
                     </div>
-                    <div class="form-group" id="email">
-                        <label for="email">Correo</label>
+                    <div class="form-group pb-2" id="email"> 
+                        <label for="email">Correo electrónico</label>
                         <input type="email" name="email" class="form-control" id="email" placeholder="Ingrese su email">
                         <div class="invalid-feedback">
                             
                         </div>
                     </div>
-                    <div class="form-group" id="password">
+                    <div class="form-group pb-2" id="password">
                         <label for="password">Contraseña</label>
                         <input type="password" name="password" class="form-control" id="password" placeholder="Ingrese su contraseña">
                         <div class="invalid-feedback">
@@ -21,7 +21,7 @@
                         </div>
                     </div>
                     <div class="form-group">
-                        <button type="submit" class="btn btn-primary form-control">Submit</button>
+                        <button type="submit" class="btn btn-primary form-control">Ingresar</button>
                     </div>
                     <div class="form-group" id="alert"></div>
                 </form>
