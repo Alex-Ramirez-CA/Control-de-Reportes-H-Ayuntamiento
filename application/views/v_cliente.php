d2631ab43cf0ba754dfa828cb3fae287bab78a3d
Luis Gerardo Sanchez Diaz <liger3799@gmail.com>
2022-03-23 12:17:08 -0600

    Agregué la validación para cuando el usuario no tenga reportes en determinado status

--- application/views/v_cliente.php
+++ application/views/v_cliente.php
@@ -11,24 +11,33 @@
             <h3><b>Reportes pendientes</b></h3>
         </div>
 
-        <div class="columna-pendiente">   
-            <?php foreach($pendientes as $item): ?>
-                <div class="card">
-                    <div class="card-title">
-                        <h5><?= $item->titulo; ?></h5>
-                    </div>
-                    <div class="card-body">
-                        <div class="texto-medio">
-                            <p class="atiende"></p>                        
-                            <p class="departamento"></p>                        
+        <div class="columna-pendiente">
+
+            <?php 
+                if (empty($pendientes)) {
+                    echo 'Cuanto vacio';
+                }else{
+                    foreach($pendientes as $item):
+            ?>
+                    <div class="card">
+                        <div class="card-title">
+                            <h5><?= $item->titulo; ?></h5>
                         </div>
-                        <div class="fecha">
-                            <h5>Creado</h5>
-                            <p><?= $item->fecha_apertura; ?></p>
+                        <div class="card-body">
+                            <div class="texto-medio">
+                                <p class="atiende"><b>Atendido por: </b>Por asignar</p>                        
+                                <p class="departamento"><b>Departamento: </b>Por asignar</p>                        
+                            </div>
+                            <div class="fecha">
+                                <h5>Creado</h5>
+                                <p><?= $item->fecha_apertura; ?></p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            <?php endforeach; ?>   
+            <?php 
+                    endforeach; 
+                }
+            ?>   
         </div>
     </div>
 
@@ -39,23 +48,31 @@
         </div>
         
         <div class="columna-proceso">   
-            <?php foreach($en_proceso as $item): ?>
-                <div class="card">
-                    <div class="card-title">
-                        <h5><?= $item->titulo; ?></h5>
-                    </div>
-                    <div class="card-body">
-                        <div class="texto-medio">
-                            <p class="atiende">Atendido por <?= $item->encargado; ?> </p>                        
-                            <p class="departamento"><?= "Departamento: ".$item->departamento; ?></p>                        
+            <?php
+                if (empty($en_proceso)) {
+                    echo 'Cuanto vacio';
+                }else{ 
+                    foreach($en_proceso as $item): 
+            ?>
+                        <div class="card">
+                            <div class="card-title">
+                                <h5><?= $item->titulo; ?></h5>
+                            </div>
+                            <div class="card-body">
+                                <div class="texto-medio">
+                                    <p class="atiende"><b>Atendido por: </b><?= $item->encargado; ?> </p>                        
+                                    <p class="departamento"><b>Departamento: </b><?=$item->departamento; ?></p>                        
+                                </div>
+                                <div class="fecha">
+                                    <h5>Creado</h5>
+                                    <p><?= $item->fecha_apertura; ?></p>
+                                </div>
+                            </div>
                         </div>
-                        <div class="fecha">
-                            <h5>Creado</h5>
-                            <p><?= $item->fecha_apertura; ?></p>
-                        </div>
-                    </div>
-                </div>
-            <?php endforeach; ?>   
+            <?php 
+                    endforeach; 
+                }    
+            ?>   
         </div>
 
     </div>
