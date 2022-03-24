ba4143a2638b64a6f3cd1331b8825edb37eb927d
Luis Gerardo Sanchez Diaz <liger3799@gmail.com>
2022-03-24 10:41:34 -0600

    Barra de busqueda y poder visualizar reportes

--- assets/js/Buscador/buscador.js
+++ assets/js/Buscador/buscador.js
@@ -1,26 +1,45 @@
 (function($) {
-    $("#search").keyup(function(ev) {
-        let search = $('#search').val();
-        console.log(search);
+    $("#search").keyup(function(ev) { 
+        if($('#search').val()){
+            let search = $('#search').val();
+            console.log(search);
+            $.ajax({
+                url: 'cliente/buscar_incidencia',
+                type: 'POST',
+                data: { search },
+                success: function(data) {
+                    let incidencias = JSON.parse(data);
+                    let template = "";
 
-        $.ajax({
-            url: 'cliente/buscar_incidencia',
-            type: 'POST',
-            data: { search },
-            success: function(data) {
-                let incidencias = JSON.parse(data);
-                let template = '';
-
-                incidencias.forEach(element => {
-                    template += `<li>
-                        ${element.titulo}
-                    </li>`;
-                });
-
-                $('#opciones-buscar').html(template);
-
+                    if (incidencias){
+                        incidencias.forEach(element => {
+                            template += `<a class="autocompletado" href="#" idCard="${element.id_incidencia}">
+                            ${element.titulo}
+                            </a>`;
+                        });
+                        $('#opciones-buscar').html(template);
+                    }
+                    
                 //window.location.replace(json.url);
-            },
-        });        
+                }
+            });
+        }else{
+            template = "";
+        }        
     });
+
+    $(document).on('click', '.card', function(){
+        let elemento = $(this)[0];
+        let id_incidencia = $(elemento).attr('idCard');
+        $.post('cliente/visualizar_reporte', {id_incidencia});
+        console.log(id_incidencia);
+    });
+
+    $(document).on('click', '.autocompletado', function(){
+        let elemento = $(this)[0]; 
+        let id_incidencia = $(elemento).attr('idCard');
+        $.post('cliente/visualizar_reporte', {id_incidencia});
+        console.log(id_incidencia);
+    });
+
 })(jQuery)
