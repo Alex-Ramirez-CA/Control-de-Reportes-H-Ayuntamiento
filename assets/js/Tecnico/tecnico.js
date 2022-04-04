feaa7c4afb8d2026280a7e715b81a480764e4a97
Luis Gerardo Sanchez Diaz <liger3799@gmail.com>
2022-04-04 12:13:34 -0500

    Agregué los botnes de la tarjeta para el técnico

---
+++ assets/js/Tecnico/tecnico.js
@@ -0,0 +1,18 @@
+(function($) {
+    //Función para cuando le de click al boton de ver
+    $(document).on('click', '.ver', function(){
+        let elemento = $(this)[0];
+        let id_incidencia = $(elemento).attr('idReporte');
+        console.log(id_incidencia);
+        $.post('reporte/visualizar_reporte', {id_incidencia}, function(response){
+            let json = JSON.parse(response);
+            window.open(json.url);
+        });
+    });
+
+    //Función para el boton de atender incidencia
+    $(document).on('click', '.atender', function(){
+        $('.mensaje').css({'visibility':'visible'});
+    });
+
+})(jQuery)
