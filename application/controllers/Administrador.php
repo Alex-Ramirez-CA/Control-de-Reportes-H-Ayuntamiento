b41700f5110f59b321355a38e31f838644edc865
Carlos Alejandro <emiramirez991@gmail.com>
2022-04-13 18:19:03 -0500

    Acomodo de funcion filtrar_incidencias

--- application/controllers/Administrador.php
+++ application/controllers/Administrador.php
@@ -54,37 +54,21 @@ class Administrador extends CI_Controller {
 	
 	public function filtrar_incidencias()
 	{
-		// if($this->session->has_userdata('id_rol') && $this->session->userdata('id_rol') == 3) {
+		if($this->session->has_userdata('id_rol') && $this->session->userdata('id_rol') == 3) {
 			
-		// 	$status = 0;
-		// 	$res_0 = $this->Incidencia->get_incidenciasAdmin($status);
-		// 	// Obtener incidencias en proceso
-		// 	$status = 1;
-		// 	$res_1 = $this->Incidencia->get_incidenciasAdmin($status);
-		// 	// Obtener incidencias en proceso
-		// 	$status = 2;
-		// 	$res_2 = $this->Incidencia->get_incidenciasAdmin($status);
-		
-		// 	$data = array(
-		// 		'pendientes' => $res_0,
-		// 		'en_proceso' => $res_1,
-		// 		'finalizados' => $res_2,
-		// 	);
-		// 	echo json_encode($data);
-        // } else {
-        //     // Si no hay datos de sesion redireccionar a login
-        //     redirect('login');
-        // }
-		// $data = $this->Incidencia->get_incidenciasFiltradas(0, NULL, NULL, NULL, NULL, NULL, NULL);
-		// $status, $direccion, $dependencia, $equipo, $departamento
-		$pendientes = $this->Incidencia->get_incidenciasStatusCero(0, 2, 1, 2, 2);
-		$en_proceso = $this->Incidencia->get_incidenciasStatusUno(1, 2, 1, 1, 3);
-		$data = array(
-					'pendientes' => $pendientes,
-					'en_proceso' => $en_proceso,
-					'finalizados' => false,
-				);
-		echo json_encode($data);
+			$pendientes = $this->Incidencia->get_incidenciasStatusCero(0, 2, 1, 2, 2);
+			$en_proceso = $this->Incidencia->get_incidenciasStatusUno(1, 2, 1, 1, 3);
+			$data = array(
+						'pendientes' => $pendientes,
+						'en_proceso' => $en_proceso,
+						'finalizados' => false,
+					);
+			echo json_encode($data);
+			
+        } else {
+            // Si no hay datos de sesion redireccionar a login
+            redirect('login');
+        }
 	}
 
 }
