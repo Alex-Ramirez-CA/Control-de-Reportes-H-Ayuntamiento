1b731ed855df234cbee4d0d10cf2b18b0dcef1a2
Carlos Alejandro <emiramirez991@gmail.com>
2022-05-28 09:19:07 -0500

    Usuario inactivo no puede ingresar al sistema

--- application/models/Auth.php
+++ application/models/Auth.php
@@ -9,7 +9,8 @@ class Auth extends CI_Model {
             ->select("b.no_empleado, b.email, b.nombre, b.apellido_paterno, b.apellido_materno, a.id_rol, a.nombre as rol_nombre, b.id_departamento")
             ->from("rol a")
             ->join("usuario b", "a.id_rol = b.id_rol")
-            ->where(array('b.email' => $usuario, 'b.password' => md5($password)), 1)
+            ->where(array('b.email' => $usuario, 'b.password' => md5($password), 'b.status' => 1))
+            ->limit(1)
             ->get();
 
         if(!$data->result()) {
