0776722712cc0d9155ed26970eddf68a8820ac09
Carlos Alejandro <emiramirez991@gmail.com>
2022-05-28 13:22:54 -0500

    base_url local

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://sire.colima.gob.mx/Control-de-Reportes-H-Ayuntamiento/';
+$config['base_url'] = 'http://localhost/Control-de-Reportes-H-Ayuntamiento/';
 
 /*
 |--------------------------------------------------------------------------
